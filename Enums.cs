97a09c22ee283a50e0d2175595b4c9a542abefd4
aaa-RaymondKhoo <raymond.e.khoo@protonmail.com>
2020-08-24 20:57:03 -0700

    adding enum for ship occupationType

---
+++ Enums.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace General_Quarters.Models
+{
+    public enum OccupationType
+    {
+        [Description("o")]
+        Empty,
+
+        [Description("B")]
+        Battleship,
+
+        [Description("C")]
+        Cruiser,
+
+        [Description("D")]
+        Destroyer,
+
+        [Description("S")]
+        Submarine,
+
+        [Description("A")]
+        Carrier,
+
+        [Description("X")]
+        Hit,
+
+        [Description("M")]
+        Miss
+    }
+
+    public enum ShotResult
+    {
+        Miss,
+        Hit
+    }
+
+}
--- Models/Ship.cs
+++ Models/Ship.cs
@@ -1,4 +1,9 @@
 using System.ComponentModel.DataAnnotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace General_Quarters.Models
 {
@@ -8,5 +13,7 @@ namespace General_Quarters.Models
         public int ShipId { get; set; }
         public string Name {get;set;}
         public int ShipLength {get;set;}
+        public int Hits {get;set;}
+        public OccupationType OccupationType {get;set;}
     }
 }
