8d246eec151c5984cf29ebd20cb9ff1e30ba100b
aaa-RaymondKhoo <raymond.e.khoo@protonmail.com>
2020-08-24 21:07:22 -0700

    adding grid coordinate

--- Models/Grid.cs
+++ Models/Grid.cs
@@ -1,11 +1,21 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
 
 namespace General_Quarters.Models
 {
     public class Grid
     {
-        public char X { get; set; }
-        public int Y { get; set; }
+        public int Row { get; set; }
+        public int Column { get; set; }
+
+        public Grid(int row, int column)
+        {
+            Row = row;
+            Column = column;
+        }
     }
 }
