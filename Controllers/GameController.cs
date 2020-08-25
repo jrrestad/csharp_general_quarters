345207aa47d85395ad69c5c9c33c5cc70ae553f6
Jonas Restad <jrrestad@gmail.com>
2020-08-25 13:45:39 -0700

    Added GameController, Game/User/JoinGame models, Main.cshtml and routing Methods

---
+++ Controllers/GameController.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using General_Quarters.Models;
+
+namespace General_Quarters.Controllers
+{
+    public class GameController : Controller
+    {
+        private int? uid
+        {
+            get
+            {
+                return HttpContext.Session.GetInt32("UserId");
+            }
+        }
+        private GQContext db;
+        public GameController(GQContext context)
+        {
+            db = context;
+        }
+        
+        [HttpGet("/main/dashboard/{gameId}")]
+        public IActionResult DashBoard(int gameId)
+        {
+            if (uid == null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            return View("Dashboard");
+        }
+    }
+}
