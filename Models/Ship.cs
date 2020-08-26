2ae1f899ebc6e463a6acaf39c7263a259ba66679
aaa-RaymondKhoo <raymond.e.khoo@protonmail.com>
2020-08-26 13:33:50 -0700

    Adding more ship for future use by commenting out
    Adding and commenting more ship in case use for future

--- Models/Ship.cs
+++ Models/Ship.cs
@@ -16,4 +16,45 @@ namespace General_Quarters.Models
         public int Hits {get;set;}
         public OccupationType OccupationType {get;set;}
     }
+
+    // public class Cruiser : Ship
+    // {
+    //     public Cruiser()
+    //     {
+    //         Name = "Cruiser";
+    //         ShipLength = 3;
+    //         ShipType = 'C';
+    //     }
+    // }
+
+    // public class Battleship : Ship
+    // {
+    //     public Battleship()
+    //     {
+    //         Name = "Battleship";
+    //         ShipLength = 4;
+    //         ShipType = 'B';
+    //     }
+    // }
+
+    // public class Carrier : Ship
+    // {
+    //     public Carrier()
+    //     {
+    //         Name = "Carrier";
+    //         ShipLength = 5;
+    //         ShipType = 'A';
+    //     }
+    // }
+
+    // public class Submarine : Ship
+    // {
+    //     public Submarine()
+    //     {
+    //         Name = "Submarine";
+    //         ShipLength = 3;
+    //         ShipType = 'S';
+    //     }
+    // }
+
 }
