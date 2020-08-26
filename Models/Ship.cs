568636c7054ec2c717b03f95cbdf1bfb679d8edb
Jim Reeder <reederje@gmail.com>
2020-08-26 13:48:14 -0700

    Added game logic and models

--- Models/Ship.cs
+++ Models/Ship.cs
@@ -4,9 +4,12 @@ namespace General_Quarters.Models
 {
     public class Ship
     {
-        public string Name {get;set;}
-        public int ShipLength {get;set;}
-        public char ShipType {get;set;}
+        public string Name { get; set; }
+        public int ShipLength { get; set; }
+        public int ShipHealth {get;set;}
+        public int ShipType { get; set; }
+        public bool IsDead {get;set;}
+        public Coordinates SLocation {get;set;}
     }
 
     public class Destroyer : Ship
@@ -15,7 +18,54 @@ namespace General_Quarters.Models
         {
             Name = "Destroyer";
             ShipLength = 2;
-            ShipType = 'D';
+            ShipHealth = 2;
+            ShipType = 3;
+            IsDead = false;
+
+        }
+    }
+    public class Crusier : Ship
+    {
+        public Crusier()
+        {
+            Name = "Crusier";
+            ShipLength = 3;
+            ShipHealth = 3;
+            ShipType = 4;
+            IsDead = false;
+        }
+    }
+    public class Submarine : Ship
+    {
+        public Submarine()
+        {
+            Name = "Submarine";
+            ShipLength = 3;
+            ShipHealth = 3;
+            ShipType = 5;
+            IsDead = false;
+        }
+    }
+    public class Battleship : Ship
+    {
+        public Battleship()
+        {
+            Name = "Battleship";
+            ShipLength = 4;
+            ShipHealth = 4;
+            ShipType = 6;
+            IsDead = false;
+        }
+    }
+    public class Carrier : Ship
+    {
+        public Carrier()
+        {
+            Name = "Carrier";
+            ShipLength = 5;
+            ShipHealth = 5;
+            ShipType = 7;
+            IsDead = false;
         }
     }
 }
