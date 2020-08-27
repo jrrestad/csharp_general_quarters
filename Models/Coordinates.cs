b64a3d0bf68c7e6bec6c9b101f90c7f1ae188b65
Jim Reeder <reederje@gmail.com>
2020-08-27 08:22:54 -0700

    added ship placement

--- Models/Coordinates.cs
+++ Models/Coordinates.cs
@@ -1,7 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace General_Quarters.Models
 {
     public class Coordinates
     {
+        [Key]
+        public int CoordID {get;set;}
         public int X { get; set; }
         public int Y { get; set; }
         public Coordinates(int x, int y)
